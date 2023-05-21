83b8d42e8b3f8227485012ac3ca3b1146f437ad1
ArtemZan <arttema9@gmail.com>
2023-05-21 19:40:57 +0300

    Init

---
+++ Gyro.cpp
@@ -0,0 +1,90 @@
+#include "Gyro.h"
+
+void Gyroscope::readRaw(double& accX, double& accY, double& accZ, double& gyroX, double& gyroY, double& gyroZ) {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(MPU, 6, true);
+  accX = (Wire.read() << 8 | Wire.read()) / 16384.0;
+  accY = (Wire.read() << 8 | Wire.read()) / 16384.0;
+  accZ = (Wire.read() << 8 | Wire.read()) / 16384.0;
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x43);
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(MPU, 6, true);
+  gyroX = (Wire.read() << 8 | Wire.read()) / 131.0;
+  gyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
+  gyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
+}
+
+void Gyroscope::calcError() {
+  auto trials = 200;
+
+  accXError = 0;
+  accYError = 0;
+  gyroXError = 0;
+  gyroYError = 0;
+  gyroZError = 0;
+
+  for (auto c = 0; c < trials; ++c) {
+    double accX, accY, accZ, gyroX, gyroY, gyroZ;
+    readRaw(accX, accY, accZ, gyroX, gyroY, gyroZ);
+
+    accXError += atan(accY / sqrt(pow(accX, 2) + pow(accZ, 2))) * 180 / PI;
+    accYError += atan(-accX / sqrt(pow(accY, 2) + pow(accZ, 2))) * 180 / PI;
+    gyroXError += gyroX;
+    gyroYError += gyroY;
+    gyroZError += gyroZ;
+  }
+
+  accXError /= trials;
+  accYError /= trials;
+  gyroXError /= trials;
+  gyroYError /= trials;
+  gyroZError /= trials;
+}
+
+void Gyroscope::initialize() {
+  Wire.begin();
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission(true);
+
+  // Wire.beginTransmission(MPU);
+  // Wire.write(0x1C);
+  // Wire.write(4);
+  // Wire.endTransmission(true);
+
+  // Wire.beginTransmission(MPU);
+  // Wire.write(0x1B);
+  // Wire.write(0x10);
+  // Wire.endTransmission(true);
+
+  calcError();
+}
+
+void Gyroscope::read(double& roll, double& pitch, double& yaw) {
+  double accX, accY, accZ, gyroX, gyroY, gyroZ;
+  readRaw(accX, accY, accZ, gyroX, gyroY, gyroZ);
+
+  double prevTime = currTime;
+  currTime = millis();
+  double elapsedTime = (currTime - prevTime) / 1000;
+
+  double accXAngle = atan(accY / sqrt(pow(accX, 2) + pow(accZ, 2))) * 180 / PI - accXError;
+  double accYAngle = atan(-accX / sqrt(pow(accY, 2) + pow(accZ, 2))) * 180 / PI - accYError;
+  gyroXAngle += (gyroX - gyroXError) * elapsedTime;
+  gyroYAngle += (gyroY - gyroYError) * elapsedTime;
+  gyroZAngle += (gyroZ - gyroZError) * elapsedTime;
+
+  roll = COMPLEMENTARY_FILTER * gyroXAngle + (1 - COMPLEMENTARY_FILTER) * accXAngle;
+  pitch = COMPLEMENTARY_FILTER * gyroYAngle + (1 - COMPLEMENTARY_FILTER) * accYAngle;
+  yaw = gyroZAngle;
+}
+}
+;
