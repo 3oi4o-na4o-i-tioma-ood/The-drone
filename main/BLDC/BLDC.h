3feefae9f54f1879865e5d3f0b423a1656bcab73
admin <artem.zankovskiy@infinno.eu>
2024-06-15 16:05:30 +0300

    Playing with BLDC

---
+++ main/BLDC/BLDC.h
@@ -0,0 +1,10 @@
+typedef struct {
+    int GPIO;
+    float throttle; // 0 to 1
+    int channel;
+} BLDC;
+
+
+void BLDC_create(BLDC* bldc, int gpio, int channel);
+
+void BLDC_set_throttle(BLDC* bldc, float throttle);
--- main/drone.c
+++ main/drone.c
@@ -9,6 +9,7 @@
 #include "esp_system.h"
 #include "esp_log.h"
 #include "linear_algebra.h"
+#include "BLDC.h"
 
 #define I2C_MASTER_SCL_IO 22      /*!< gpio number for I2C master clock */
 #define I2C_MASTER_SDA_IO 21      /*!< gpio number for I2C master data  */
@@ -19,8 +20,17 @@ static const char *TAG = "mpu6050 test";
 
 static mpu6050_handle_t mpu6050 = NULL;
 
+typedef struct
+{
+    BLDC motorTop;
+    BLDC motorRight;
+    BLDC motorBottom;
+    BLDC motorLeft;
+} Drone;
 
-void mpu6050_get_normal(VectorFloat* const normal)
+Drone drone;
+
+void mpu6050_get_normal(VectorFloat *const normal)
 {
     int16_t quaternion[4];
 
@@ -40,20 +50,33 @@ void mpu6050_get_normal(VectorFloat* const normal)
 
 void app_main()
 {
-    //vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // vTaskDelay(1000 / portTICK_PERIOD_MS);
     printf("Hi");
 
-    mpu6050_init(&mpu6050, I2C_MASTER_NUM, I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO, I2C_MASTER_FREQ_HZ);
+    // mpu6050_init(&mpu6050, I2C_MASTER_NUM, I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO, I2C_MASTER_FREQ_HZ);
 
+    BLDC_create(&drone.motorTop, 25, LEDC_CHANNEL_0);
+    BLDC_create(&drone.motorRight, 26, LEDC_CHANNEL_1);
+    BLDC_create(&drone.motorBottom, 27, LEDC_CHANNEL_2);
+    BLDC_create(&drone.motorLeft, 33, LEDC_CHANNEL_3);
+
+    float throttle = 0.0;
+    int dir = 1;
     while (true)
     {
+        BLDC_set_throttle(&drone.motorTop, throttle);
         vTaskDelay(500 / portTICK_PERIOD_MS);
+        throttle += 0.1 * dir;
+        if (throttle >= 0.7 || throttle <= 0.1)
+        {
+            dir *= -1;
+        }
 
-        mpu6050_get_current_FIFO_packet(mpu6050);
+        // mpu6050_get_current_FIFO_packet(mpu6050);
 
-        VectorFloat normal;
-        mpu6050_get_normal(&normal);
+        // VectorFloat normal;
+        // mpu6050_get_normal(&normal);
 
-        ESP_LOGI(TAG, "x:%.2f, y:%.2f, z:%.2f\n", normal.x, normal.y, normal.z);
+        // ESP_LOGI(TAG, "x:%.2f, y:%.2f, z:%.2f\n", normal.x, normal.y, normal.z);
     }
 }
